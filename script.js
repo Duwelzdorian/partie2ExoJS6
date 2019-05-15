787ae7de8698a004288ab2b8b89af8b314d107c7
Duwelzdorian <duwelzdorian@gmail.com>
2019-05-15 12:18:06 +0200

    exo6

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeImage_2(element) {
+  // concatener les attributs de l'element pour remplacer par un autre image
+  element.src = 'images/' + element.id + '_2.jpg';
+}
+function changeImage(element) {
+  element.src = 'images/' + element.id + '.jpg';
+}
